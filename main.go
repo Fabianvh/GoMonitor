3b65295321c1ffa1c1a6f54a588be03df269cae0
Fabian van Hekke <fabianvanhekke@hotmail.nl>
2022-06-23 09:48:47 +0200

    Update main.go

--- main.go
+++ main.go
@@ -10,31 +10,31 @@ import (
 	//Import Github van orginele code.
 )
 
-var configPath = flag.String("config", "configs/default.json", "configuration file")
-var logPath = flag.String("log", "logs/from-"+time.Now().Format("2006-01-02")+".log", "log file")
-var address = flag.String("http", ":8080", "address for http server")
-var nolog = flag.Bool("nolog", false, "disable logging to file only")
-var logfilter = flag.String("logfilter", "", "text to filter log by (both console and file)")
+var configPath = flag.String("config", "configs/default.json", "configuration file")              //Geeft pad naar configfile aan
+var logPath = flag.String("log", "logs/from-"+time.Now().Format("2006-01-02")+".log", "log file") //Geeft pad aan naar logfile
+var address = flag.String("http", ":8080", "address for http server")                             //Geeft de poort aan waar de webserver op gaat draaien
+var nolog = flag.Bool("nolog", false, "disable logging to file only")                             //Als de functie true word aangezet word logging uitgezet
+var logfilter = flag.String("logfilter", "", "text to filter log by (both console and file)")     //Maakt text naar een logfile
 
 func main() {
 	flag.Parse()
 	jsonData, err := ioutil.ReadFile(*configPath) //Laad configuratie file van /configs
 	if err != nil {
-		panic("Fout bij lezen van configuratiefile")
+		panic("Fout bij lezen van configuratiefile") //Foutmelding message bij niet kunnen laden van de configfile
 	}
 
-	if *nolog == true {
+	if *nolog == true { //Als de logfile uitstaat, start hij functie logger disable
 		logger.Disable()
 	}
 
-	if *logfilter != "" {
+	if *logfilter != "" { //Logfilter
 		logger.Filter(*logfilter)
 	}
 
-	logger.SetFilename(*logPath)
+	logger.SetFilename(*logPath) //Zet bestandnaam van de logfile
 
-	config := NewConfig(jsonData)
-	monitor := NewMonitor(config)
-	go gossm.RunHttp(*address, monitor)
-	gossm.Run() //Start monitoring tool uit bestand
+	config := NewConfig(jsonData)       //Laad configuratiefile in functie Newconfig
+	monitor := NewMonitor(config)       //Laad monitor in NewMonitor
+	go gossm.RunHttp(*address, monitor) //Zet HTTP server aan met address en monitoring functie
+	gossm.Run()                         //Start monitoring tool
 }
